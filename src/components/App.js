ce4396dff723b333816b7d25e76c647943f1ff45
unknown <dshipuk@yahoo.com>
2022-04-18 14:55:12 -0700

    added add 1

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,16 @@ import './App.css';
 import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
 
+import { addOne } from '../actions'
+
+
 function App() {
   const [ state, dispatch ] = useReducer(reducer, initialState);
+
+
+  const handleOneClick = () => {
+    dispatch(addOne());
+  }
 
   return (
     <div className="App">
@@ -31,7 +39,7 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
+              <CalcButton value={1} onClick={handleOneClick} />
               <CalcButton value={2}/>
               <CalcButton value={3}/>
             </div>
